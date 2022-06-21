242f3f1b02d344380c17359179fd75c91c50a0de
suresh <suresh@192.168.1.4>
2022-06-21 12:07:36 +0530

    First Commit

---
+++ Day3_JoinedTableInheritance1/src/com/inheritance3/Manager.java
@@ -0,0 +1,21 @@
+package com.inheritance3;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mgr_store3")
+public class Manager extends Employee 
+{
+	private static final long serialVersionUID=1L;
+	private String deptName;
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+
+}
